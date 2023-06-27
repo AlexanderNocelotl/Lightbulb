c489a78901a6e32dfaa2d0a89a0edadad899322c
TXT-82 <txt.hackathon@TXTs-MBP-5.askey.com>
2023-06-26 21:06:21 -0700

    lightbulb

---
+++ app.js
@@ -0,0 +1,8 @@
+const img= document.getElementById("on")
+function on(){
+img.src ="https://cdn.mos.cms.futurecdn.net/HaPnm6P7TZhPQGGUxtDjAg-320-80.jpg"
+}
+
+function off(){
+    img.src="https://c1.wallpaperflare.com/preview/825/807/809/bulb-electricity-energy-glass.jpg"
+}
